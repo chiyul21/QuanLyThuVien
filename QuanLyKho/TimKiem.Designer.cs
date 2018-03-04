2dfa21413215a391e68ae3a311b9baf03bdfd6dc
chiyul21 <chiyulsnsd@gmail.com>
2018-03-04 23:03:48 +0700

    QuanLyKho

---
+++ QuanLyKho/TimKiem.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace QuanLyKho.GUI
+{
+    partial class TimKiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenHH = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.dgvTimKiem = new System.Windows.Forms.DataGridView();
+            this.maHH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tenHH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maNCC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soluong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTimKiem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Cyan;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(545, 116);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tìm Kiếm Hàng Hoá";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Từ Tìm";
+            // 
+            // txtTenHH
+            // 
+            this.txtTenHH.Location = new System.Drawing.Point(327, 139);
+            this.txtTenHH.Name = "txtTenHH";
+            this.txtTenHH.Size = new System.Drawing.Size(189, 22);
+            this.txtTenHH.TabIndex = 2;
+            this.txtTenHH.TextChanged += new System.EventHandler(this.txtTenHH_TextChanged);
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(12, 180);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(75, 31);
+            this.btnTimKiem.TabIndex = 3;
+            this.btnTimKiem.Text = "Tìm Kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvTimKiem
+            // 
+            this.dgvTimKiem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTimKiem.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.maHH,
+            this.tenHH,
+            this.maNCC,
+            this.soluong});
+            this.dgvTimKiem.Location = new System.Drawing.Point(3, 216);
+            this.dgvTimKiem.Name = "dgvTimKiem";
+            this.dgvTimKiem.RowTemplate.Height = 24;
+            this.dgvTimKiem.Size = new System.Drawing.Size(545, 150);
+            this.dgvTimKiem.TabIndex = 4;
+            // 
+            // maHH
+            // 
+            this.maHH.DataPropertyName = "maHH";
+            this.maHH.HeaderText = "Mã HH";
+            this.maHH.Name = "maHH";
+            // 
+            // tenHH
+            // 
+            this.tenHH.DataPropertyName = "tenHH";
+            this.tenHH.HeaderText = "Tên HH";
+            this.tenHH.Name = "tenHH";
+            // 
+            // maNCC
+            // 
+            this.maNCC.DataPropertyName = "maNCC";
+            this.maNCC.HeaderText = "Mã NCC";
+            this.maNCC.Name = "maNCC";
+            // 
+            // soluong
+            // 
+            this.soluong.DataPropertyName = "soluong";
+            this.soluong.HeaderText = "Số Lượng";
+            this.soluong.Name = "soluong";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cách tìm";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Mã Hàng Hoá",
+            "Tên Hàng Hoá"});
+            this.comboBox1.Location = new System.Drawing.Point(107, 139);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // TimKiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 423);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dgvTimKiem);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.txtTenHH);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "TimKiem";
+            this.Text = "TimKiem";
+            this.Load += new System.EventHandler(this.TimKiem_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTimKiem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenHH;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.DataGridView dgvTimKiem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maHH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tenHH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maNCC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soluong;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ QuanLyKho/TimKiem.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLyKho.GUI
+{
+    public partial class TimKiem : Form
+    {       
+        string strConn = @"Data Source=NGOCXINH\SQLEXPRESS;Initial Catalog=QuanLyKho;Integrated Security=True";
+        SqlConnection conn = new SqlConnection();
+        private void LoadData()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * from HangHoa", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvTimKiem.DataSource = dt;
+        }
+
+        //public void LoadDataByTimKiem()
+        //{
+        //SqlDataAdapter da = new SqlDataAdapter("SELECT * from HangHoa where TenHH like N'%" + txtTenHH.Text + "%'", conn);
+        //DataTable dt = new DataTable();
+        //da.Fill(dt);
+        //    dgvTimKiem.DataSource = dt;
+        //}
+       
+        public TimKiem()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(strConn);
+            conn.Open();
+            LoadData();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Mã Hàng Hoá")
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * from HangHoa where MaHH like N'%" + txtTenHH.Text + "%'", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvTimKiem.DataSource = dt;
+                //dgvTimKiem.DataSource = dt ("select * from HangHoa where MaHH like '%" + txtTenHH.Text.Trim() + "%'");
+            }
+            if (comboBox1.Text == "Tên Hàng Hoá")
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * from HangHoa where TenHH like N'%" + txtTenHH.Text + "%'", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvTimKiem.DataSource = dt;                
+            }
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void TimKiem_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = "Mã Hàng Hoá";
+            LoadData();
+        }
+
+        private void txtTenHH_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Mã Hàng Hoá")
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * from HangHoa where MaHH like N'%" + txtTenHH.Text + "%'", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvTimKiem.DataSource = dt;
+                //dgvTimKiem.DataSource = dt ("select * from HangHoa where MaHH like '%" + txtTenHH.Text.Trim() + "%'");
+            }
+            if (comboBox1.Text == "Tên Hàng Hoá")
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * from HangHoa where TenHH like N'%" + txtTenHH.Text + "%'", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvTimKiem.DataSource = dt;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
